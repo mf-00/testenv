887fc7b8926bac78a18d1e2ea7812cee58bf465c
mf-00 <mf.yang@hotmail.com>
2016-09-22 09:57:21 +0800

    Update main.go
    print environment variables

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!</h1>\n")
+    for _, e := range os.Environ() {
+        fmt.Fprintf(w, e)
+    }
 }
 
 func main() {
